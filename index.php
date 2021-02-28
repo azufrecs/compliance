680f57715ed4afa7021722ad7c17fea8d7b6ad2d
AZUfre, Computer Solutions <azufre.cs@gmail.com>
2021-02-27 22:07:35 -0500

    arreglando pagina inicial

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<!-- Etiquetas <meta> obligatorias para Bootstrap -->
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="img/favicon.png">
+		<title>Ventas - Cumplimiento</title>
+
+		<!-- Enlazando el CSS de Bootstrap -->
+		<link href="css/bootstrap.css" rel="stylesheet" media="screen">
+		<link href="css/hover.css" rel="stylesheet" media="screen">
+		<link href="css/main.css" rel="stylesheet" media="screen">
+		<link href="css/rotate.css" rel="stylesheet" media="screen">
+		<link href="css/icons.css" rel="stylesheet" media="screen">
+		<!-- Enlazando el CSS de Bootstrap -->
+		
+		<!-- Enlazando el JavaScript de Bootstrap -->
+		<script src="js/jquery-3.5.1.js"></script>
+		<script src="js/bootstrap.js"></script>
+		<script src="js/rotate.js"></script>
+		<script src="js/jquery_rotate.js"></script>
+		<script src="js/icons.js"></script>
+		<!-- Enlazando el JavaScript de Bootstrap -->
+	</head>
+  
+	<body>
+		<div class="container">
+			<!-- BEGIN CONTENT -->
+			<div class="content">
+				<!-- Inicio de Encabezado -->
+				<div class="row">
+					<div class="col" align="center"><img src="img/logo.png"></div>
+				</div>	
+				
+				<div align="center" style="color:#666666; font-size:24px">Ventas y Cumplimiento de Farmacias</div>
+				<div align="center" style="color:#eeeeee; font-size:4px">&nbsp;</div>
+				<!-- Fin de Encabezado -->
+			
+
+			</div>
+			<!-- FINISH CONTENT -->
+
+			<!-- BEGIN FOOTER -->
+			<footer>
+				<div class='col-md-12' align='center'>
+					<div class='btn-group btn-group-sm'>
+						<a class='btn btn-success' href='https://www.cmw.smcsalud.cu' role='button' data-placement='top'>Web SMC Cmw</a>
+						<a class='btn btn-primary' href='config' role='button' data-placement='top'><i class="fas fa-cog"></i>&nbsp;Configuraci&oacute;n</a>
+					</div>
+					<div align="center" style="color:#666666; font-size:12px">Servicios M&eacute;dicos Cubanos, Sucursal Camag&uuml;ey<div>
+				</div>
+			</footer>
+			<!-- FINISH FOOTER -->
+		</div>
+		<!-- FINISH CONTAINER -->
+	</body>
+</html>
